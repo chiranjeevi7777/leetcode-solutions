99b9c6cce6255c73674b77650968153fb6d2de01
Chiranjeevi R <165194631+chiranjeevi7777@users.noreply.github.com>
2026-07-16 18:51:04 +0000

    Sync LeetCode submission - Merge Strings Alternately (python3)

---
+++ problems/merge_strings_alternately/solution.py
@@ -0,0 +1,11 @@
+class Solution:
+    def mergeAlternately(self, word1: str, word2: str) -> str:
+        l = max(len(word1), len(word2))
+        result = ""
+        for i in range(l):
+            if i < len(word1):
+                result += f"{word1[i]}"
+            if i < len(word2):
+                result += f"{word2[i]}"
+        return result
+        
