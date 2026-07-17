5a3c77673f5d5d6a6f60694e6a71fb7b64312183
Chiranjeevi R <165194631+chiranjeevi7777@users.noreply.github.com>
2026-07-17 18:04:41 +0000

    Sync LeetCode submission - Valid Parentheses (python3)

---
+++ problems/valid_parentheses/solution.py
@@ -0,0 +1,21 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        stack = []
+        pairs = {
+        ")": "(",
+        "]": "[",
+        "}": "{"
+        }
+        for ch in s:
+            if ch in ["(", "[", "{"]:
+                stack.append(ch)
+            else:
+                if not stack or stack[-1] != pairs[ch]:
+                    return False
+                stack.pop()
+        result = False
+
+
+        return len(stack) == 0
+
+        
