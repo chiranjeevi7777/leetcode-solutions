017c3b7944d6e5ccd7657ef49e47d458d163d0ca
Chiranjeevi R <165194631+chiranjeevi7777@users.noreply.github.com>
2026-07-29 06:00:22 +0000

    Sync LeetCode submission - Valid Palindrome (python3)

---
+++ problems/valid_palindrome/solution.py
@@ -0,0 +1,12 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        c = "".join(char.lower() for char in s if char.isalnum())
+        l = 0
+        r = len(c) - 1
+
+        while l < r:
+            if c[l] != c[r]:
+                return False
+            l += 1
+            r -= 1
+        return True
